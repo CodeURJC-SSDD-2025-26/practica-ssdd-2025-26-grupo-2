a964d436de09cd055fb7c642ec0135dc872d4722
yadirareimundez <yadirareiq@gmail.com>
2026-03-18 14:04:18 +0100

    creación de la carpeta backend

---
+++ backend/src/main/java/com/ssdd/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssdd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
