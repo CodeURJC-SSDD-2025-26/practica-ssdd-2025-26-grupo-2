b6edf1e0cfab3b2dbf76888e65eac0b46ca3e9eb
VanessaFernandes19 <v.fernandes.2023@alumnos.urjc.es>
2026-04-09 10:13:45 +0200

    repositorios hechos

---
+++ backend/src/main/java/com/ssdd/backend/repository/TravelRepository.java
@@ -0,0 +1,9 @@
+package com.ssdd.backend.repository;
+
+import com.ssdd.backend.model.Travel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TravelRepository extends JpaRepository<Travel, Long> {
+}
