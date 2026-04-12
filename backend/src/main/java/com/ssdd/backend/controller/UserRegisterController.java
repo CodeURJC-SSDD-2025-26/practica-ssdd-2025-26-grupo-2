95ebe4b1e06c679aad3e9da4103e9c82590c0ca4
ahdd2 <ahdd2003@gmail.com>
2026-04-12 20:43:19 +0200

    hacer que aparezca de forma correcta el script del viaja a... y que coincida con la imagen. Moví el script al footer.

--- backend/src/main/java/com/ssdd/backend/controller/UserRegisterController.java
+++ backend/src/main/java/com/ssdd/backend/controller/UserRegisterController.java
@@ -1,6 +1,6 @@
 package com.ssdd.backend.controller;
 
-<<<<<<< HEAD
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,21 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssdd.backend.service.UserService;
-=======
-import com.ssdd.backend.service.UserService;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
->>>>>>> dbc6a6dbc6f30f959acc4779c3b2717e190f0201
 
 @Controller
 public class UserRegisterController {
