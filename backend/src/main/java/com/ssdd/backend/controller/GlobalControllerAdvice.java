827434439d795e7bf28ef7605a260039e602cb5d
m.abdallah.2023 <m.abdallah.2023@alumnos.urjc.es>
2026-04-14 19:17:30 +0200

    buscador principal implementado

--- backend/src/main/java/com/ssdd/backend/controller/GlobalControllerAdvice.java
+++ backend/src/main/java/com/ssdd/backend/controller/GlobalControllerAdvice.java
@@ -2,6 +2,8 @@ package com.ssdd.backend.controller;
 
 import com.ssdd.backend.model.User;
 import com.ssdd.backend.service.UserService;
+import com.ssdd.backend.service.TravelService;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
@@ -14,23 +16,23 @@ import java.util.Optional;
 public class GlobalControllerAdvice {
 
     @Autowired
-    private UserService userService; 
+    private UserService userService;
+    @Autowired
+    private TravelService travelService;
 
     @ModelAttribute
     public void addAttributes(Model model, HttpServletRequest request) {
         Principal principal = request.getUserPrincipal();
+        model.addAttribute("viajes", travelService.getAllTravels());
 
         if (principal != null) {
             model.addAttribute("logged", true);
             model.addAttribute("userName", principal.getName());
             model.addAttribute("admin", request.isUserInRole("ROLE_ADMIN"));
 
-
             Optional<User> user = userService.findByEmail(principal.getName());
-            //para poder hacer {{user.imagenPerfil}}
             user.ifPresent(u -> model.addAttribute("user", u));
-            // ----------------------------------
-            
+
         } else {
             model.addAttribute("logged", false);
         }
--- backend/src/main/java/com/ssdd/backend/repository/TravelRepository.java
+++ backend/src/main/java/com/ssdd/backend/repository/TravelRepository.java
@@ -1,9 +1,18 @@
 package com.ssdd.backend.repository;
 
 import com.ssdd.backend.model.Travel;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, Long> {
+    @Query("SELECT t FROM Travel t WHERE t.pais = :country " +
+       "AND t.maxPlazas >= :travelers " +
+       "AND (t.fechaInicio >= :start AND t.fechaFin <= :end)")
+List<Travel> findCustomTrips(String country, int travelers, LocalDate start, LocalDate end);
 }
