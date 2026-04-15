162db274b72a68aa47003159f3a99211aa04e1f0
m.abdallah.2023 <m.abdallah.2023@alumnos.urjc.es>
2026-04-15 10:25:04 +0200

    correo actualizado

--- backend/src/main/java/com/ssdd/backend/repository/TravelRepository.java
+++ backend/src/main/java/com/ssdd/backend/repository/TravelRepository.java
@@ -13,6 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface TravelRepository extends JpaRepository<Travel, Long> {
     @Query("SELECT t FROM Travel t WHERE t.pais = :country " +
             "AND t.maxPlazas >= :travelers " +
-            "AND (t.fechaInicio >= :start OR t.fechaFin <= :end)")
+            "AND (t.fechaInicio <= :start AND t.fechaFin >= :end)")
     List<Travel> findCustomTrips(String country, int travelers, LocalDate start, LocalDate end);
 }
