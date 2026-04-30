5ad29f37d4c0caf97f1f9b6161f3696dd97eeec8
ahdd2 <ahdd2003@gmail.com>
2026-04-30 14:06:40 +0200

    ReviewDTO,Mapper y RestController

---
+++ backend/src/main/java/com/ssdd/backend/dto/ReviewMapper.java
@@ -0,0 +1,29 @@
+package com.ssdd.backend.dto;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.ssdd.backend.model.Review;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+
+
+    @Mapping(source = "autor.id", target = "autorId")
+    @Mapping(source = "viaje.id", target = "viajeId")
+    ReviewDTO toDTO(Review review);
+
+    // Método para convertir listas completas (muy útil para el Controller)
+    List<ReviewDTO> toDTOs(Collection<Review> reviews);
+
+    // Convertimos de DTO a Entidad
+    // Ignoramos los objetos completos porque los buscaremos en el Service por su ID
+    @Mapping(target = "autor", ignore = true)
+    @Mapping(target = "viaje", ignore = true)
+    Review toEntity(ReviewDTO reviewDTO);
+}
+
+
